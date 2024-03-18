84f088b7ad720616e006ab14d05846d6b9020f79
MohammedAnis <mohammedanismultani@gmail.com>
2024-03-18 09:28:15 +0530

    calculator

---
+++ calculator/script.js
@@ -0,0 +1,28 @@
+let display = document.getElementById("inputBox")
+let buttons = document.querySelectorAll("button")
+
+let buttonsArray = Array.from(buttons)
+
+let str = ""
+buttonsArray.forEach(btn =>{
+    btn.addEventListener("click", (e) =>{
+        if(e.target.innerHTML == "DEL"){
+            str = str.substring(0,str.length-1)
+            display.value = str
+        }
+        else if(e.target.innerHTML == "AC")
+        {
+            str = "";
+            display.value = str;
+        }
+        else if(e.target.innerHTML == "=")
+        {
+            str = eval(str);
+            display.value = str;
+        }
+        else{
+        str = str + e.target.innerHTML
+        display.value = str;
+        }
+    })
+})
